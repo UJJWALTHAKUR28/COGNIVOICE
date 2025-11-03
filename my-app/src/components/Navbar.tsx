768d9e5f845dfa712097003cb707ca98d85b8704
UJJWAL THAKUR <146047866+UJJWALTHAKUR28@users.noreply.github.com>
2025-11-03 19:45:06 +0530

    Update Navbar.tsx

--- my-app/src/components/Navbar.tsx
+++ my-app/src/components/Navbar.tsx
@@ -1,30 +1,16 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { FaHome, FaMicrophone, FaYoutube } from "react-icons/fa";
 import Link from "next/link";
 import { GiSoundWaves } from "react-icons/gi";
 import { usePathname, useRouter } from "next/navigation";
-import { useTheme } from "next-themes";
 import { useAuth } from "@/context/authContext";
 
 const Navbar: React.FC = () => {
   const pathname = usePathname();
   const router = useRouter();
-  const { theme, setTheme } = useTheme();
   const { user, logout } = useAuth();
-
-  const [isDarkMode, setIsDarkMode] = useState<boolean>(theme === "dark");
-
-  useEffect(() => {
-    setIsDarkMode(theme === "dark");
-  }, [theme]);
-
-  const toggleTheme = () => {
-    const newTheme = isDarkMode ? "light" : "dark";
-    setTheme(newTheme);
-    setIsDarkMode(!isDarkMode);
-  };
 
   const handleLogout = () => {
     logout();
@@ -53,20 +39,32 @@ const Navbar: React.FC = () => {
 
   return (
     <header className="fixed top-0 left-0 w-full z-50 bg-white shadow-md">
-      <nav className="bg-white dark:bg-gray-900 text-black dark:text-white p-4 flex justify-between items-center border-b border-teal-600">
+      <nav className="bg-white text-black p-4 flex justify-between items-center border-b border-teal-600">
         <div>
           <h1 className="text-2xl font-bold bg-gradient-to-r from-teal-900 via-teal-500 to-teal-800 bg-clip-text text-transparent font-mono">
             Cognivoice
           </h1>
         </div>
+
         <div className="flex items-center space-x-6">
           <NavLink href="/" icon={FaHome} label="Home" />
           {user ? (
             <>
               <NavLink href="/voice" icon={FaMicrophone} label="PulseTap" />
-              <NavLink href="/voicecontiousRecording" icon={GiSoundWaves} label="MoodStream" />
-              <NavLink href="/youtube-emotion" icon={FaYoutube} label="YouTube VoiceScan" />
-              <button onClick={handleLogout} className="ml-4 hover:text-red-500 font-semibold">
+              <NavLink
+                href="/voicecontiousRecording"
+                icon={GiSoundWaves}
+                label="MoodStream"
+              />
+              <NavLink
+                href="/youtube-emotion"
+                icon={FaYoutube}
+                label="YouTube VoiceScan"
+              />
+              <button
+                onClick={handleLogout}
+                className="ml-4 hover:text-red-500 font-semibold"
+              >
                 Logout
               </button>
             </>
@@ -76,7 +74,6 @@ const Navbar: React.FC = () => {
               <NavLink href="/signup" label="Signup" />
             </>
           )}
-         
         </div>
       </nav>
     </header>
